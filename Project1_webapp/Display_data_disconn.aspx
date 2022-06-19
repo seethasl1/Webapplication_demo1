6744d557b839dea6e26beabc2aa0aeb757821259
seethalakshmi s <seethasl1@gmail.com>
2022-06-19 19:15:43 +0530

    display page

---
+++ Project1_webapp/Display_data_disconn.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Mainmaster.Master" AutoEventWireup="true" CodeBehind="Display_data_disconn.aspx.cs" Inherits="Project1_webapp.Display_data_disconn" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <br />
+    
+
+</asp:Content>
---
+++ Project1_webapp/Display_data_disconn.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Project1_webapp
+{
+    public partial class Display_data_disconn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string strcon = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                SqlConnection con = new SqlConnection(strcon);
+                SqlDataAdapter da = new SqlDataAdapter("select uname,city,gender from login", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text= ex.Message;
+            }
+        }
+    }
+}
